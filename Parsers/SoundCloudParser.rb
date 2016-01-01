6c2c4896176d931675ce66cc6417375a6f8ddce3
Sunmock Yang <sunmock@live.ca>
2015-12-31 23:40:14 -0500

    Added soundcloud parsing
    - Using soundcloud ruby api

--- Parsers/SoundCloudParser.rb
+++ Parsers/SoundCloudParser.rb
@@ -1,8 +1,20 @@
 # SoundCloudParser.rb
+require "soundcloud"
 
-class SoundCloudParser < SpiderParser
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+
+class SoundCloudSpider < SpiderParser
+	# register the client
+	@@client = SoundCloud.new(:client_id => CLIENT_ID)
 	def initialize(url)
+		# call the resolve endpoint with a track url
+		track = @@client.get('/resolve', :url => url)
+
 		@url = url
+		@title = track.title
+		@artist = track.user.username
+		@thumbnailURL = track.artwork_url
 	end
 
 	def self.can_parse?(url)
