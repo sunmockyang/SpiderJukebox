093c5118655ccb75e4da19122d374e673d4869f5
Sunmock Yang <sunmock@live.ca>
2016-01-03 22:33:11 -0500

    Documentation, formatting, bugs
    - Added documentation
    - Fixed some parser caching redundancy
    - Moved stuff around so cmd functionality is at bottom

--- SpiderJukebox.rb
+++ SpiderJukebox.rb
@@ -1,6 +1,53 @@
 require 'optparse'
 require_relative "SpiderParser"
 
+class SpiderJukebox
+	def self.parse(url, options={})
+		track = nil
+		force = options.delete(:force)
+
+		decode_parser = self.get_parser(url)
+		if decode_parser # Valid parser found in Parsers/*.rb
+			parser = decode_parser
+			track = parser.parse(url)
+			track.set_metadata(options)
+		elsif force # No valid parsers found, but force create track based on options
+			track = SpiderTrack.new(url: url)
+			track.set_metadata(options)
+		else # TODO: add verbose option
+			puts "No available parsers for: " + url
+		end
+
+		return track
+	end
+
+	def self.parse_to_json(url, options={})
+		return self.parse(url, options).to_json
+	end
+
+	@@parser_cache = {}
+	def self.get_parser(url)
+		# Find the correct parser type for the url
+		decode_parser_type = SpiderParser.descendants.select{|available_parser| available_parser.can_parse?(url)}.first
+		parser = nil
+		
+		if !decode_parser_type.nil?
+			# Cache parser if an instance doesn't exist
+			if !@@parser_cache.has_key?(decode_parser_type)
+				@@parser_cache[decode_parser_type] = decode_parser_type.new(url)
+			end
+			parser = @@parser_cache[decode_parser_type]
+		end
+
+		return parser
+	end
+
+	def self.clear_parser_cache
+		@@parser_cache.clear
+	end
+end
+
+# For use with command line args
 usage_banner = "Usage: ruby SpiderJukebox.rb [url] [options]"
 
 if ARGV.empty?
@@ -24,53 +71,6 @@ OptionParser.new do |opts|
 		exit
 	end
 end.parse!
-
-class SpiderJukebox
-	@@parser_cache = {}
-
-	def self.parse(url, options={})
-		track = nil
-		force = options.delete(:force)
-		decode_parser = self.get_parser(url)
-		if decode_parser
-			parser = decode_parser
-			track = parser.parse(url)
-			track.set_metadata(options)
-		elsif force
-			# Invalid url but there are options we can use
-			track = SpiderTrack.new(url: url)
-			track.set_metadata(options)
-		else
-			puts "No available parsers for: " + url
-		end
-
-		return track
-	end
-
-	def self.parse_to_json(url, options={})
-		return self.parse(url, options).to_json
-	end
-
-	def self.get_parser(url)
-		decode_parser_type = SpiderParser.descendants.select{|available_parser| available_parser.can_parse?(url)}.first
-		parser = nil
-		
-		if !decode_parser_type.nil?
-			if @@parser_cache[decode_parser_type]
-				parser = @@parser_cache[decode_parser_type]
-			else
-				parser = decode_parser_type.new(url)
-				@@parser_cache[decode_parser_type] = decode_parser_type.new(url)
-			end
-		end
-
-		return parser
-	end
-
-	def self.clear_parser_cache
-		@@parser_cache.clear
-	end
-end
 
 url = ARGV[0] || ""
 if url || !options.empty?
